41efba0642b1af4a57412b76c9025ac4675f7ad2
Phanor Coll <phanorcoll@gmail.com>
2017-11-14 09:27:31 -0400

    added the left sidebar container for the groups and users

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import TopBar from '../components/top_bar.jsx';
+import LeftBar from '../components/left_bar.jsx';
+
 export default class App extends Component {
     render() {
         return (
             <div>
                 <TopBar />
+                <div className="container-fluid">
+                    <div className="row">
+                        <LeftBar />
+                        <div className="col">
+                            chat
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
